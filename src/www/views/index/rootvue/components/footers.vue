6c6efb9ad03edbb1f83fd0acf034ad7acd49a07a
xiaozhuxiaozhi <2085188234@qq.com>
2018-09-15 16:16:50 +0800

    添加按钮，输入框。

--- src/www/views/index/rootvue/components/footers.vue
+++ src/www/views/index/rootvue/components/footers.vue
@@ -1,15 +1,62 @@
 <template>
-	<div>Footer组件</div>
+	<div class="box">
+    <Input  placeholder="Enter something..." clearable style="width: 200px" />
+    <Select  style="width:200px">
+        <Option v-for="item in cityList" :value="item.value" :key="item.value">{{ item.label }}</Option>
+    </Select>
+    <Button type="primary">Search</Button>
+    <Button type="info" @click="modal7 = true">Add</Button>
+       <Modal
+        title="对话框标题"
+        v-model="modal7"
+        :closable="false">
+        <p>对话框内容</p>
+        <p>对话框内容</p>
+        <p>对话框内容</p>
+    </Modal>
+</div>
+
 </template>
 
 <script type="text/javascript">
 	export default {
 		name:'footers',
 		data(){
-			return {}
+			return {
+				modal7: false,
+				 cityList: [
+                    {
+                        value: 'New York',
+                        label: 'New York'
+                    },
+                    {
+                        value: 'London',
+                        label: 'London'
+                    },
+                    {
+                        value: 'Sydney',
+                        label: 'Sydney'
+                    },
+                    {
+                        value: 'Ottawa',
+                        label: 'Ottawa'
+                    },
+                    {
+                        value: 'Paris',
+                        label: 'Paris'
+                    },
+                    {
+                        value: 'Canberra',
+                        label: 'Canberra'
+                    }
+                ]
+			}
 		}
 	}
 </script>
 <style scoped>
-	
+	.box{
+		margin: 20px;
+		text-align: center;
+	}
 </style>
